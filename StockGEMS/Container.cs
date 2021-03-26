3abdf23cc3811628bacaaa0f389686f4ab8989f4
Eugene Lymar <Eugene.Lymar@bentley.com>
2021-03-26 15:42:19 -0400

    Introduced naive Container class

---
+++ StockGEMS/Container.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockGEMS
+{
+    public class Container
+    {
+        private IDictionary<Type, Type> _mappings; 
+        private IDictionary<Type, object> _singletons;
+        
+        public Container()
+        {
+            _mappings = new Dictionary<Type, Type>();
+            _singletons = new Dictionary<Type, object>(); 
+        }
+
+        public void RegisterSingleton(Type interf, Type concrete)
+        {
+            _mappings.Add(interf, concrete); 
+        }
+
+        public object GetObject(Type interfaceType)
+        {
+            // Base Case 1 (already created an instance)
+            if (_singletons.ContainsKey(interfaceType)) 
+                return _singletons[interfaceType];
+
+            if (!_mappings.ContainsKey(interfaceType))
+                throw new ApplicationException($"Type {interfaceType} does not have a registered concrete class");
+
+            var concreteType = _mappings[interfaceType]; 
+            var constructors = concreteType.GetConstructors();
+            if (constructors.Length != 1) throw new ApplicationException($"Only one constructor supported. Got {constructors.Length}");
+            var constructor = constructors[0];
+            var parameters = constructor.GetParameters();
+
+            // Base Case 2 (Constructor has no parameters, trivial construction)
+            if (parameters.Length == 0)
+            {
+                _singletons.Add(interfaceType, constructor.Invoke(null));
+                return _singletons[interfaceType]; 
+            }
+
+            // Recursive Case (For each parameter of the constructor GetObject recursively)
+            object[] arguments = new object[parameters.Length];
+            for (int i = 0; i <parameters.Length; i++)
+            {
+                arguments[i] = GetObject(parameters[i].ParameterType); 
+            }
+            _singletons.Add(interfaceType, constructor.Invoke(arguments));
+            return _singletons[interfaceType];
+        }
+    }
+}
--- StockGEMS/Program.cs
+++ StockGEMS/Program.cs
@@ -8,6 +8,17 @@ namespace StockGEMS
         {
             Console.WriteLine("Hello World!");
             // Build 
+            Container container = new Container();
+            container.RegisterSingleton(typeof(ILogger), typeof(ConsoleLogger)); 
+            container.RegisterSingleton(typeof(IStrategy), typeof(Strategy));
+            container.RegisterSingleton(typeof(IQuoteService), typeof(QuoteService)); 
+            container.RegisterSingleton(typeof(IStockGEMS), typeof(StockGEMS)); 
+            container.RegisterSingleton(typeof(IStockBroker), typeof(StockBroker));
+
+            IStockGEMS stockGems = (IStockGEMS)container.GetObject(typeof(IStockGEMS));
+            stockGems.Run(); 
+
+            /*
             IQuoteService quoteService = new QuoteService(
                 new ConsoleLogger()
             );
@@ -19,6 +30,7 @@ namespace StockGEMS
             );
             IStockGEMS stockTrader = new StockGEMS(quoteService, strategy, purchaser);
             stockTrader.Run(); 
+            */
         }
     }
 }
